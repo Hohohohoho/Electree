03f614e4eda31fba767a3ad1e8900228a0c129cf
Park Changho <pch4463@gmail.com>
2014-11-30 22:04:34 +0900

    changho

--- Electree/src/com/sgen/electree/newgroup/AddGroupActivity.java
+++ Electree/src/com/sgen/electree/newgroup/AddGroupActivity.java
@@ -1,18 +1,28 @@
 package com.sgen.electree.newgroup;
 
-import com.sgen.electree.R;
-import com.sgen.electree.R.layout;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.sgen.electree.R;
 
 public class AddGroupActivity extends Activity {
+	Button addAlert;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_group);
+		
+		addAlert = (Button)findViewById(R.id.addGroupBtn);
+        addAlert.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getBaseContext(), NewGroupResultActivity.class));
+			}
+		});
 	}
 }
