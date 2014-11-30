292e4743ecafcdc5976097034a796ecec876faa0
이지영 <violentjy@naver.com>
2014-11-30 16:04:37 +0900

    mainChange

--- Electree/gen/com/sgen/electree/R.java
+++ Electree/gen/com/sgen/electree/R.java
@@ -1072,6 +1072,7 @@ containing a value of this type.
     }
     public static final class id {
         public static final int ButtonSet=0x7f05003d;
+        public static final int acc_btn=0x7f050042;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1086,14 +1087,10 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003e;
-        public static final int button2=0x7f05003f;
-        public static final int button3=0x7f050040;
-        public static final int button4=0x7f050041;
-        public static final int button5=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
+        public static final int device_btn=0x7f050040;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1102,8 +1099,10 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int group_btn=0x7f05003f;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
+        public static final int home_btn=0x7f05003e;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
@@ -1136,6 +1135,7 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int signinMainBtn=0x7f050047;
         public static final int split_action_bar=0x7f05001e;
+        public static final int stat_btn=0x7f050041;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
